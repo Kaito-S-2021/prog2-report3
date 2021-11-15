3d5bb5d227234cc9229d09b25f5ceb18203d67e8
Suzuki Kaito <e215727@ie.u-ryukyu.ac.jp>
2021-11-15 12:44:01 +0900

    Step3

--- app/src/test/java/jp/ac/uryukyu/ie/e215727/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215727/EnemyTest.java
@@ -19,7 +19,7 @@ class EnemyTest {
             demoHero.attack(slime); // over kill
         }
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero.gethitpoint());
     }
 
 }
